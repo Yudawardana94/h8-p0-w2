b42f7f0226a797f3f4684482ef2e6c182a229005
yudawardana94 <yuda.wardana40@gmail.com>
2019-02-26 22:35:36 +0700

    add the challenge date

---
+++ date.js
@@ -0,0 +1,45 @@
+
+  var day = 25
+  var month = 3
+  var year = 2015
+
+//code
+switch (month) {
+    case 1:
+        console.log(day +' January '+ year)
+        break;
+    case 2:
+        console.log(day +' February '+ year);
+        break;
+    case 3:
+        console.log(day +' March '+ year);
+        break;
+    case 4:
+        console.log(day +' April '+year)
+        break;
+    case 5:
+        console.log(day +' May '+year)
+        break
+    case 6:
+        console.log(day +' June' +year);
+        break;
+    case 7:
+        console.log(day +' July' +year);
+        break;
+    case 8:
+        console.log(day +' Agustus' +year);
+        break;
+    case 9:
+        console.log(day +' September' +year);
+        break;
+    case 10:
+        console.log(day +' Oktober' +year);
+        break;
+    case 11:
+        console.log(day +' November' +year);
+        break;
+    case 12:
+        console.log(day +' December' +year);
+        break;
+}
+

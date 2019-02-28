6b191983427ac0e3dfdb70e2246fe7935c5023ab
yudawardana94 <yuda.wardana40@gmail.com>
2019-02-28 11:52:30 +0700

    memperbaiki

--- proxytia.js
+++ proxytia.js
@@ -4,11 +4,11 @@ var role = 'priest'
 if( name && role === ''){
   console.log("Hi " + name + ", choose your role to play a game")
 } else if( name && role === 'warrior'){
-  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo '+ role +' '+ name + ', you can attack with your weapon"')
+  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo warrior '+ name + ', you can attack with your weapon"')
 } else if( name && role === 'priest'){
-  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo '+ role +' '+ name + ', you will help your wounded friends"')
+  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo priest '+ name + ', you will help your wounded friends"')
 } else if( name && role === 'wizard'){
-  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo '+ role +' '+ name + ', make a miracle that help your victory"')
+  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo wizard '+ name + ', make a miracle that help your victory"')
 } else{
   console.log('"Name must be filled"')
 }

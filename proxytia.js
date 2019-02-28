b3badbaf2015b2d8b424d5e5f569eb14c8b1d9c9
yudawardana94 <yuda.wardana40@gmail.com>
2019-02-28 11:40:31 +0700

    memperbaiki penamaan

--- proxytia.js
+++ proxytia.js
@@ -1,14 +1,14 @@
-var name = 'koko'
-var role = 'wizard'
+var name = 'nina'
+var role = 'priest'
 
-if( name === 'yuda' && role === ''){
-  console.log("Hi yuda, choose your role to play a game")
-} else if( name === 'yuda' && role === 'warrior'){
-  console.log('"welcome to proxytia world, yuda"\n' + '"hallo warior yuda, you can attack with your weapon"')
-} else if( name === 'rinda' && role === 'priest'){
-  console.log('"welcome to proxytia world, rinda"\n' + '"hallo priest rinda, you will helpyour wounded friends"')
-} else if( name === 'koko' && role === 'wizard'){
-  console.log('"welcome to proxytia world, koko"\n' + '"hallo wizard koko, make a miracle that help your victory"')
+if( name && role === ''){
+  console.log("Hi " + name + ", choose your role to play a game")
+} else if( name && role === 'warrior'){
+  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo '+ role +' '+ name + ', you can attack with your weapon"')
+} else if( name && role === 'priest'){
+  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo '+ role +' '+ name + ', you will help your wounded friends"')
+} else if( name && role === 'wizard'){
+  console.log('"welcome to proxytia world, ' + name +'"\n' + '"hallo '+ role +' '+ name + ', make a miracle that help your victory"')
 } else{
   console.log('"Name must be filled"')
 }
